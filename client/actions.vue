e8d784c443a3282c6dfb7a9cbe3edaa938df4f2b
Andrew Mao <mizzao@gmail.com>
2017-08-21 12:43:05 -0400

    Show active player

--- client/actions.vue
+++ client/actions.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
     <h2>Player Actions</h2>
-    Currently selected: {{ selectedPlayer }}
+    
+    <h3>
+      Currently selected: {{ selectedPlayer }}
+    </h3>
+
+    <h3 v-if="player">
+      Wealth: {{player.value}}
+    </h3>
+
   </div>
 </template>
 
 <script>
 export default {
   props: [ 'selectedPlayer' ],
+  data: function() {
+    return {
+      player: null
+    }
+  },
+  meteor: {
+    player: {
+      params() {
+        return {
+          id: this.selectedPlayer
+        };
+      },
+      update({id}) {
+        return Nodes.findOne(id);
+      }
+    }
+  },
   methods: {
     
   }
